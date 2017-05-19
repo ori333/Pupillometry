3a5fe9274a039c327b5c263148c6d3bdcdfd4c76
Val-Apana <valentin.siderskiy@apana.com>
2017-05-19 00:02:51 -0700

    First IR Video
    First IR Video

--- main.py
+++ main.py
@@ -15,41 +15,45 @@ videosDir = fileDir + '\Videos'
 #change directory for videos
 os.chdir(videosDir)
 
-cap = cv2.VideoCapture('MVI_0077_clipped.mp4')
+cap = cv2.VideoCapture('IR_Video.mp4')
 
 while(cap.isOpened()):
 	ret, imgColor = cap.read()
 	if ret==True:
-		img = cv2.cvtColor(imgColor,cv2.COLOR_BGR2GRAY)
-		img = cv2.medianBlur(img,5)
-		cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+		
+		# cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
 
 		#Find contour
 		imgray = cv2.cvtColor(imgColor,cv2.COLOR_BGR2GRAY)
-		ret,thresh = cv2.threshold(imgray,40,255,0)
-		cv2.imshow('threshold',thresh)
+		ret,thresh = cv2.threshold(imgray,100,255,0)
+		# cv2.imshow('threshold',thresh)
+		#cv2.imshow('gray',imgray)
+
+		#find the iris contour
+		#4 for this image
+		_, contours, _ = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+		lastContour = np.empty(contours[1].shape)
+		for contour in contours:
+			if cv2.contourArea(contour) > 10000 and cv2.contourArea(contour) < 20000: 
+				cv2.drawContours(imgColor, contour, -1, (0,255,0), 3)
+				lastContour = contour
 
 
-		circles = cv2.HoughCircles(thresh,cv2.HOUGH_GRADIENT,1,100, circles=2, param1=1,param2=10,minRadius=50,maxRadius=140)
 
-		if circles != None:
-			circles = np.uint16(np.around(circles))
 
-			for i in circles[0,:]:
-			    # draw the outer circle
-			    cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
-			    # draw the center of the circle
-			    cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+		# circles = cv2.HoughCircles(thresh,cv2.HOUGH_GRADIENT,1,100, circles=2, param1=100,param2=15,minRadius=60,maxRadius=100)
 
-			#find the iris contour
-			#4 for this image
-			_, contours, _ = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-			lastContour = np.empty(contours[1].shape)
-			for contour in contours:
-				if cv2.contourArea(contour) > 10000:
-					cv2.drawContours(imgColor, contour, -1, (0,255,0), 3)
-					lastContour = contour
-					# cv2.circle(imgColor,(i[0],i[1]),2,(0,0,255),3)
+		# if circles != None:
+		# 	circles = np.uint16(np.around(circles))
+
+		# 	# for i in circles[0,:]:
+		# 	#     # draw the outer circle
+		# 	#     cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+		# 	#     # draw the center of the circle
+		# 	#     cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+		# 	# cv2.imshow('detected circles',cimg)
+
+		# 			# cv2.circle(imgColor,(i[0],i[1]),2,(0,0,255),3)
 
 		#print contour
 
@@ -80,8 +84,10 @@ while(cap.isOpened()):
 
 
 		#Countours output array of arrays
-		cv2.imshow('original image',imgColor)
-		#cv2.imshow('detected circles',cimg)
+		imgColorSmall = cv2.resize(imgColor, (0,0), fx=0.5, fy=0.5)
+		imgraySmall = cv2.resize(imgray,(0,0), fx=0.5, fy=0.5)
+		cv2.imshow('original image',imgColorSmall)
+		cv2.imshow('imgray',imgraySmall)
 
 		plt.ion()
 		plt.clf()
